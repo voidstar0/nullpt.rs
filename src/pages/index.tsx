41fe58b329526af50ba648078a2db4a7b7d761dc
char <25884226+char@users.noreply.github.com>
2020-06-07 16:54:44 -0400

    Bio gone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@
 import React from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
